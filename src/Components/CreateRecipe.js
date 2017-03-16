4f02484ab17dab8d538d79c35834a00b779a8e9f
Giorgi Abuladze <g.abuladze@live.com>
2017-03-16 19:56:21 +0400

    Added basic form

--- src/Components/CreateRecipe.js
+++ src/Components/CreateRecipe.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CreateRecipe = () => (
-    <div>
-        <h1>Create Recipe</h1>
-    </div>
-)
+class CreateRecipe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            ingredients: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Create Recipe</h1>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Name"/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="ingredients">Ingredients</label>
+                        <textarea name="ingredients" id="ingredients" className="form-control" rows="10" placeholder="Ingredients, separated, by, commas"></textarea>
+                    </div>
+
+                    <button type="submit" className="btn btn-default">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default CreateRecipe;
