2a547f25d5c4f897debf04bac48f0b68e2e9b2ab
Giorgi Abuladze <g.abuladze@live.com>
2017-03-16 21:18:23 +0400

    Implemented simple list of recipes from local storage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import LocalStorage from '../Services/LocalStorage';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { recipes: [] };
+    }
+
+    componentDidMount() {
+        this.setState({ recipes: LocalStorage.get() });
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                <ul>
+                    {
+                        this.state.recipes.map((recipe, index) => {
+                            return <li key={index}>{recipe.name}</li>;
+                        })
+                    }
+                </ul>
             </div>
         );
     }
