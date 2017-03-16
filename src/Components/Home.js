63fa037effd2913d065ace6889046f1366546dc1
Giorgi Abuladze <g.abuladze@live.com>
2017-03-16 20:55:45 +0400

    Converted component to react component class

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import LocalStorage from '../Services/LocalStorage';
 
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+            </div>
+        );
+    }
+}
 
 export default Home;
