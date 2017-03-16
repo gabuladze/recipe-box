a48b947b1b99e65c820879602cd6d83a1cd80ccf
Giorgi Abuladze <g.abuladze@live.com>
2017-03-16 19:23:49 +0400

    Added basic routing to Home and CreateRecipe Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import CreateRecipe from './Components/CreateRecipe';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src='' className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/recipes/create">Add recipe</Link></li>
+                </ul>
+
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/recipes/create" component={CreateRecipe}/>
+            </div>
+        </Router>
     );
   }
 }
