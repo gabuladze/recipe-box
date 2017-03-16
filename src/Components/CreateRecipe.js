092437acad373513f13694178ff5146a48ecdc4a
Giorgi Abuladze <g.abuladze@live.com>
2017-03-16 21:17:04 +0400

    Implemented redirect on form submit

--- src/Components/CreateRecipe.js
+++ src/Components/CreateRecipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import LocalStorage from '../Services/LocalStorage';
 
 class CreateRecipe extends Component {
@@ -6,7 +7,8 @@ class CreateRecipe extends Component {
         super(props);
         this.state = {
             name: '',
-            ingredients: ''
+            ingredients: '',
+            redirectToIndex: false
         }
     }
 
@@ -19,9 +21,14 @@ class CreateRecipe extends Component {
     handleSubmit(event) {
         event.preventDefault();
         LocalStorage.save(this.state);
+        this.setState({ redirectToIndex: true });
     }
 
     render() {
+        const { redirectToIndex } = this.state;
+
+        if(redirectToIndex) return <Redirect to="/"/>;
+
         return (
             <div>
                 <h1>Create Recipe</h1>
@@ -33,12 +40,12 @@ class CreateRecipe extends Component {
                             className="form-control"
                             placeholder="Name"
                             onChange={this.handleChange.bind(this, 'name')}
-                            value={this.state.name}/>
+                            value={this.state.name} />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="ingredients">Ingredients</label>
-                        <textarea 
+                        <textarea
                             name="ingredients"
                             className="form-control"
                             rows="10"
