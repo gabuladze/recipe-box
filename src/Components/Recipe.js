fb20f74db15baa65f226a99efe51b2ec3effeaad
Giorgi Abuladze <g.abuladze@live.com>
2017-03-18 09:07:40 +0400

    Added basic logic to Recipe component

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react';
+import LocalStorage from '../Services/LocalStorage';
 
-const Recipe = () => <div></div>;
+class Recipe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { recipe: {} };
+    }
+
+    componentDidMount() {
+        this.setState({ recipe: LocalStorage.get(this.props.match.params.id) });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.recipe.name &&
+                <div>
+                    <h1>{this.state.recipe.name}</h1>
+                    <h3>Ingredients</h3>
+                    <ul>
+                        {
+                            this.state.recipe.ingredients.split(',').map((ingredient, index) => {
+                                return <li key={index}>{ingredient}</li>;
+                            })
+                        }
+                    </ul>
+                </div>
+                }
+            </div>
+        );
+    }
+}
 
 export default Recipe;
