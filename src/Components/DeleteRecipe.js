2d93ee7b5a0d4aac52ddeb16a77be017bdef55b1
Giorgi Abuladze <g.abuladze@live.com>
2017-03-19 23:07:02 +0400

    It actually works now

--- src/Components/DeleteRecipe.js
+++ src/Components/DeleteRecipe.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import LocalStorage from '../Services/LocalStorage';
 
-const DeleteRecipe = ({ match }) => {
-    return <div>DELETE</div>;
+class DeleteRecipe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { redirectToIndex: false };
+    }
+
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        LocalStorage.deleteRecipe(id);
+        this.setState({ redirectToIndex: true });
+    }
+
+    render() {
+        const { redirectToIndex } = this.state;
+        if (redirectToIndex) return <Redirect to="/" />;
+        return null;
+    }
 }
 
 export default DeleteRecipe;
